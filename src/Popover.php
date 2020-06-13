92dfcf6958c43413b91feec8a444c137d10c1b55
Igor A Tarasov <develop@dicr.org>
2020-06-13 06:43:49 +0500

    popover

--- src/Popover.php
+++ src/Popover.php
@@ -1,14 +1,7 @@
 <?php
 /**
- * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
- * @license proprietary
- * @version 31.05.20 06:12:06
- */
-
-/**
- * @author Igor A Tarasov <develop@dicr.org>
- * @version 02.05.20 18:18:45
+ * @version 13.06.20 06:43:49
  */
 
 declare(strict_types = 1);
@@ -18,23 +11,14 @@ namespace dicr\widgets;
 use yii\base\Widget;
 use yii\helpers\Html;
 use function ob_get_clean;
+use function ob_start;
+use function trim;
 
 /**
  * Виджет всплывающей подсказки.
  *
  * Позицию и ориентацию строки, а также размеры popover требуется подстроить дополнительно через css блока,
  * в котором он используется.
- *
- * Контент виджета добавляется методом обертки:
- * ```php
- * Popover::begin();
- * echo '<a href="javascript:">Ссылка со всплывашкой</a>';
- * Popover::beginPopup();
- * echo '<h3>Заголовок окна</h3>';
- * echo '<p>Текст окна</p>'
- * Popover::endPopup();
- * Popover::end();
- * ```
  *
  * @noinspection PhpUnused
  */
@@ -53,6 +37,12 @@ class Popover extends Widget
     public const POS_RIGHT = 'right';
 
     public $position = self::POS_TOP;
+
+    /** @var string|null оборачиваемый контент */
+    public $content;
+
+    /** @var string|null содержимое всплывашки */
+    public $popup;
 
     /** @var array опции виджета */
     public $options = [];
@@ -73,7 +63,7 @@ class Popover extends Widget
      */
     public function run()
     {
-        $content = trim(ob_get_clean());
+        $content = $this->content . trim(ob_get_clean());
         if (empty($content)) {
             return '';
         }
@@ -82,32 +72,14 @@ class Popover extends Widget
 
         ob_start();
         echo Html::beginTag('section', $this->options);
+
         echo $content;
+
+        if (! empty($this->popup)) {
+            echo '<div class="popup"><div class="inner">' . $this->popup . '</div></div>';
+        }
+
         echo Html::endTag('section');
-
         return ob_get_clean();
-    }
-
-    /**
-     * Начало всплывающего окна.
-     */
-    public static function beginPopup()
-    {
-        ob_start();
-    }
-
-    /**
-     * Конец всплывающего окна
-     */
-    public static function endPopup()
-    {
-        $content = ob_get_clean();
-        ?>
-        <div class="popup">
-            <div class="inner">
-                <?= $content ?>
-            </div>
-        </div>
-        <?php
     }
 }
