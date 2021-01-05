8945aa2ea6883da0eed7de72caee69dfa8108cff
Igor A Tarasov <develop@dicr.org>
2021-01-05 12:59:33 +0500

    CustomSelect rewrite

--- src/CustomSelect.php
+++ src/CustomSelect.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @author Igor A Tarasov <develop@dicr.org>
- * @version 18.12.20 12:48:00
+ * @version 05.01.21 12:51:33
  */
 
 declare(strict_types = 1);
@@ -10,6 +10,7 @@ namespace dicr\widgets;
 use yii\base\InvalidConfigException;
 
 use function is_array;
+use function is_scalar;
 use function ob_get_clean;
 use function ob_start;
 
@@ -18,92 +19,183 @@ use function ob_start;
  */
 class CustomSelect extends InputWidget
 {
-    /** @var string[] ассоциативный массив значений val => label */
-    public $values;
+    /** @var string[]|array[] ассоциативный массив значений value => item */
+    public $items;
 
-    /** @var ?string текст не выбранного значения */
+    /** @var ?array элемент пустого значения */
     public $placeholder;
 
     /**
      * @inheritDoc
      * @throws InvalidConfigException
      */
-    public function init() : void
+    public function init(): void
     {
         parent::init();
 
-        if (empty($this->options['id'])) {
-            $this->options['id'] = $this->id;
-        }
-
-        if (empty($this->values)) {
-            $this->values = [];
-        } elseif (! is_array($this->values)) {
-            throw new InvalidConfigException('values');
-        }
-
+        // placeholder
         if ($this->placeholder !== null) {
-            $this->placeholder = (string)$this->placeholder;
+            $this->placeholder = $this->parseItem($this->placeholder);
         }
 
-        Html::addCssClass($this->options, 'dicr-widget-custom-select');
+        // items
+        if (empty($this->items)) {
+            $this->items = [];
+        } elseif (! is_array($this->items)) {
+            throw new InvalidConfigException('items');
+        }
+
+        foreach ($this->items as &$item) {
+            $item = $this->parseItem($item);
+        }
+
+        unset($item);
+
+        Html::addCssClass($this->options, 'widget-custom-select');
     }
 
     /**
      * @inheritDoc
      */
-    public function run() : string
+    public function run(): string
     {
         CustomSelectAsset::register($this->view);
 
         ob_start();
         echo Html::beginTag('section', $this->options);
 
-        // определяем название поля ввода и значение
-        $inputName = null;
-        $value = null;
-
-        if ($this->hasModel()) {
-            $inputName = Html::getInputName($this->model, $this->attribute);
-            $value = (string)Html::getAttributeValue($this->model, $this->attribute);
-        } else {
-            $inputName = $this->name;
-            $value = (string)$this->value;
-        }
-
-        // определяем метку текущего значения
-        $label = $this->values[$value] ?? '';
-        if ($label === '') {
-            $label = $this->placeholder;
-        }
-
-        // кнопка открытия списка
-        echo Html::button(Html::encode($label), [
-            'class' => [($value === '') ? 'placeholder' : '']
-        ]);
+        // кнопка раскрытия
+        echo $this->renderButton();
 
         // всплывающий список
-        echo Html::beginTag('div', ['class' => 'popup']);
-
-        if ($this->placeholder !== null) {
-            echo Html::label(
-                Html::radio($inputName, $value === '', ['value' => '']) .
-                Html::encode($this->placeholder), null, ['class' => 'placeholder']
-            );
-        }
-
-        if (! empty($this->values)) {
-            foreach ($this->values as $val => $label) {
-                echo Html::label(
-                    Html::radio($inputName, $value === (string)$val, ['value' => $val]) .
-                    Html::encode($label)
-                );
-            }
-        }
-
-        echo Html::endTag('div');   // popup
+        echo $this->renderItems();
 
         echo Html::endTag('section');   // widget
+
+        return ob_get_clean();
+    }
+
+    /**
+     * Парсит элемент.
+     *
+     * @param string|array|null $item
+     * @return array
+     */
+    protected function parseItem($item): array
+    {
+        if ($item === null || is_scalar($item)) {
+            $item = [
+                'label' => (string)$item,
+                'encode' => true,
+            ];
+        }
+
+        return [
+            'label' => (string)($item['label'] ?? ''),
+            'encode' => (bool)($item['encode'] ?? false),
+            'class' => empty($item['class']) ? null : $item['class']
+        ];
+    }
+
+    /**
+     * Рендерит кнопку раскрытия элемента с текущим значением.
+     *
+     * @return string
+     */
+    protected function renderButton(): string
+    {
+        ob_start();
+
+        echo Html::beginTag('button', [
+            'type' => 'button',
+            'class' => 'widget-custom-select-button'
+        ]);
+
+        // текущее значение модели
+        $currentValue = $this->hasModel() ?
+            (string)Html::getAttributeValue($this->model, $this->attribute) :
+            (string)$this->value;
+
+        // активный элемент
+        $item = $this->items[$currentValue] ?? $this->placeholder;
+
+        // если не задан placeholder, то создаем пустой элемент
+        if ($item === null) {
+            $item = [
+                'label' => '',
+                'encode' => false,
+                'class' => null
+            ];
+        }
+
+        echo $this->renderItem($currentValue, $item);
+        echo Html::endTag('button');
+
+        return ob_get_clean();
+    }
+
+    /**
+     * Рендерит список выбора.
+     *
+     * @return string
+     */
+    protected function renderItems(): string
+    {
+        ob_start();
+        echo Html::beginTag('datalist');
+
+        if ($this->placeholder !== null) {
+            echo $this->renderItem('', $this->placeholder, 'ph');
+        }
+
+        $index = 0;
+        foreach ($this->items as $value => $item) {
+            echo $this->renderItem((string)$value, $item, $index++);
+        }
+
+        echo Html::endTag('datalist');
+
+        return ob_get_clean();
+    }
+
+    /**
+     * Рендерит элемент.
+     *
+     * @param string|int|null $value
+     * @param array $item
+     * @param ?int $index суффикс ID элемента ввода
+     * @return string
+     */
+    protected function renderItem(string $value, array $item, $index = null): string
+    {
+        // определяем название поля ввода и значение
+        if ($this->hasModel()) {
+            $inputName = Html::getInputName($this->model, $this->attribute);
+            $currentValue = (string)Html::getAttributeValue($this->model, $this->attribute);
+        } else {
+            $inputName = $this->name;
+            $currentValue = (string)$this->value;
+        }
+
+        $id = $index === null ? null : $this->id . '-' . $index;
+
+        ob_start();
+
+        if ($id !== null) {
+            echo Html::radio($inputName, $currentValue === $value, [
+                'id' => $id,
+                'value' => $value
+            ]);
+        }
+
+        $label = $item['encode'] ? Html::esc($item['label']) : $item['label'];
+        if ($label === '') {
+            $label = '&nbsp;';
+        }
+
+        echo Html::label($label, $id, [
+            'class' => $item['class'],
+        ]);
 
         return ob_get_clean();
     }
--- src/assets/custom-select/script.js
+++ src/assets/custom-select/script.js
@@ -1,57 +1,227 @@
 /*
  * @author Igor A Tarasov <develop@dicr.org>
- * @version 23.07.20 21:31:29
+ * @version 05.01.21 12:59:03
  */
 
-(function (window, $) {
-    'use strict';
+"use strict";
 
+(function (window, $) {
+    // noinspection ES6ConvertVarToLetConst
+    /** @var {string} селектор класса виджета */
+    var selector = '.widget-custom-select';
+
+    // noinspection JSUnusedGlobalSymbols
     /**
      * Искусственный элемент select.
      *
+     * Функции:
+     *
+     * $widget[0].val(), $widget.val() - получить значение
+     * $widget[0].val(val) - установить значение
+     * $widget[0].items(...) - установить новые элементы
+     * $widget.on('change', function(e, value) {}) - событие изменения
+     *
+     * @param {HTMLElement} target
      * @constructor
      */
-    function CustomSelectWidget()
+    function CustomSelectWidget(target)
     {
-        /** @var {string} селектор класса виджета */
-        const selector = '.dicr-widget-custom-select';
+        // noinspection ES6ConvertVarToLetConst
+        var self = this;
 
-        // глобальный обработчик
-        $(window.document)
-            // отменяем предыдущие обработчики
-            .off(selector)
+        self.dom = $(target);
+        self.dom.button = $('button', self.dom);
+        self.dom.list = $('datalist', self.dom);
 
-            // обработка кликов
-            .on('click' + selector, function (e) {
-                // закрываем все виджеты по клику на документ
-                $(window.document).find(selector).removeClass('active');
+        /**
+         * обновляет кнопку текущим значением выбранного элемента
+         *
+         * @param {JQuery} $input активный измененный radio input
+         */
+        self.updateValue = function ($input) {
+            if ($input.length > 0) {
+                // обновляем значение виджета
+                self.dom[0].value = $input[0].value;
 
-                // виджет на который был клик
-                const $target = $(e.target);
-                const $widget = $target.closest(selector);
-                if ($widget.length < 1) {
-                    return;
+                // noinspection ES6ConvertVarToLetConst
+                var $label = $input.next('label');
+                if ($label.length > 0) {
+                    self.dom.button.empty().append(
+                        $label.clone().removeAttr('for')
+                    );
                 }
+            }
+        };
 
-                // если клик по кнопке, то открываем виджет
-                if ($target.is('button')) {
-                    e.preventDefault();
-                    $widget.addClass('active');
-                }
-            })
+        /**
+         * Обработка клика по кнопке.
+         *
+         * @param {Event} e
+         */
+        self.handleButtonClick = function (e) {
+            e.preventDefault();
 
-            // смена текста кнопки при выборе значения
-            .on('change' + selector, selector + ' input', function () {
-                const $label = $(this).closest('label');
-                const $widget = $(this).closest(selector);
-                const $button = $('button', $widget);
+            // переключаем открытое состояние
+            self.dom.toggleClass('open');
+        };
 
-                $button.text($label.text());
-                $button.toggleClass('placeholder', $(this).val() === '');
+        /**
+         * Обработка клика по документу
+         *
+         * @param {Event} e
+         */
+        self.handleDocumentClick = function (e) {
+            // пропускаем клики по кнопке виджета
+            // noinspection ES6ConvertVarToLetConst
+            var $button = $(e.target).closest('button');
+            if ($button.length < 1 || $button[0] !== self.dom.button[0]) {
+                self.dom.removeClass('open');
+            }
+        };
+
+        /**
+         * Обработка переключения radio.
+         *
+         * @param {Event} e
+         */
+        self.handleChange = function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            // обновляем кнопку и значение элемента
+            self.updateValue($(this));
+            // эмулируем синтетическое событие change
+            self.dom.triggerHandler('change', $(this).val());
+        };
+
+        /**
+         * Установить/получить значение элемента.
+         *
+         * @param {string|undefined} value
+         * @returns {string|JQuery}
+         */
+        self.val = function (value) {
+            if (typeof value === 'undefined') {
+                return self.dom[0].value;
+            }
+
+            // noinspection ES6ConvertVarToLetConst, JSStringConcatenationToES6Template
+            var $input = $('input[value="' + value + '"]', self.dom.list);
+            // noinspection JSUnusedGlobalSymbols
+            $input[0].checked = true;
+            self.updateValue($input);
+
+            return self.dom;
+        };
+
+        // noinspection JSUnusedGlobalSymbols
+
+        /**
+         * Установить новые значения
+         *
+         * $widget[0].items(...)
+         *
+         * @param {string} name название поля ввода
+         * @param {{
+         *     label: string
+         *     encode: boolean|undefined,
+         *     class: string|undefined
+         * }[]|string[]} items значения value => string| item{label, encode}
+         */
+        self.items = function (name, items) {
+            // удаляем все элементы кроме placeholder
+            $('input:not([value=""])', self.dom.list).each(function () {
+                $(this).next('label').remove();
+                $(this).remove();
             });
+
+            // добавляем новые элементы
+            Object.keys(items).forEach(function (value, index) {
+                // noinspection ES6ConvertVarToLetConst
+                var item = items[value];
+                if (typeof item !== 'object') {
+                    item = {label: item, encode: true};
+                }
+
+                // noinspection ES6ConvertVarToLetConst,JSStringConcatenationToES6Template
+                var id = self.dom.attr('id') + '-' + index;
+
+                // noinspection ES6ShorthandObjectProperty
+                self.dom.list.append(
+                    $('<input/>', {type: 'radio', id: id, name: name, value: value})
+                );
+
+                // noinspection ES6ConvertVarToLetConst
+                var $label = $('<label/>', {for: id, class: item.class || undefined});
+                $label[item.encode ? 'text' : 'html'](item.label);
+                self.dom.list.append($label);
+            });
+
+            // устанавливаем значение первого элемента
+            // noinspection ES6ConvertVarToLetConst
+            var $input = $('input:first', self.dom.list);
+            // noinspection JSUnusedGlobalSymbols
+            $input[0].checked = true;
+            self.updateValue($input);
+        };
+
+        // временно включаем datalist
+        self.dom.addClass('open');
+
+        // ожидаем отрисовки
+        window.requestAnimationFrame(function () {
+            // определяем максимальную ширину
+            // noinspection ES6ConvertVarToLetConst
+            var maxWidth = 0;
+            $('label', self.dom.list).each(function () {
+                if (this.offsetWidth > maxWidth) {
+                    maxWidth = this.offsetWidth;
+                }
+            });
+
+            // прячем
+            self.dom.removeClass('open');
+
+            // устанавливаем ширину кнопки по максимальной
+            self.dom.button.css('width', maxWidth);
+        });
+
+        // начальное значение элемента
+        self.updateValue($('input:checked', self.dom.list));
+
+        // клики по кнопке
+        // noinspection JSStringConcatenationToES6Template
+        self.dom.button.off(selector).on('click' + selector, self.handleButtonClick);
+
+        // клики по документу
+        // noinspection JSStringConcatenationToES6Template
+        $(window.document).on('click' + selector, self.handleDocumentClick);
+
+        // изменение выбранного значения
+        // noinspection JSStringConcatenationToES6Template
+        self.dom.list.off(selector).on('change' + selector, 'input', self.handleChange);
+
+        // получение/установка значения
+        self.dom[0].val = self.val;
+
+        // установка нового списка элементов
+        self.dom[0].items = self.items;
     }
 
-    // регистрируем функцию
-    window.app = window.app || {};
-    window.app.dicrWidgetCustomSelect = window.app.dicrWidgetCustomSelect || new CustomSelectWidget();
+    /**
+     * Инициализация всех виджетов на странице
+     */
+    function init()
+    {
+        $(window.document).off(selector);
+
+        $(selector, window.document).each(function () {
+            $(this).data('widget', new CustomSelectWidget(this));
+        });
+    }
+
+    // при загрузке страницы
+    $(init);
+
+    // при загрузке Ajax
+    $(window.document).ajaxComplete(init);
 })(window, jQuery);
