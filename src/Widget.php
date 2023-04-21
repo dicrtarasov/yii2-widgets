23c497cb0bdda0eeaf19f65857af6ddd2872ebab
Игорь Тарасов <dicrtarasov@yandex-team.ru>
2023-04-21 11:14:40 +0300

    fix Widget::registerPlugin for yiisoft/yii2-bootstrap5 2.0.4

--- src/Widget.php
+++ src/Widget.php
@@ -1,18 +1,35 @@
 <?php
 /*
- * @copyright 2019-2021 Dicr http://dicr.org
+ * @copyright 2019-2023 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license MIT
- * @version 12.08.21 22:00:50
+ * @version 21.04.23 11:11:49
  */
 
 declare(strict_types = 1);
 namespace dicr\widgets;
+
+use yii\helpers\Json;
 
 /**
  * Class Widget
  */
 class Widget extends \yii\bootstrap5\Widget
 {
-    // noop
+    /**
+     * @inheritDoc
+     */
+    protected function registerPlugin(string $name): void
+    {
+        $view = $this->getView();
+        $id = $this->options['id'];
+
+        if ($this->clientOptions !== false) {
+            $options = empty($this->clientOptions) ? '' : Json::htmlEncode($this->clientOptions);
+            $js = "jQuery('#$id').$name($options);";
+            $view->registerJs($js);
+        }
+
+        $this->registerClientEvents();
+    }
 }
